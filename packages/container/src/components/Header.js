a62ff0f15bc204608b6310688b9199586ce55726
Moshiko Basal <moshiko@bambidynamic.com>
2022-12-13 17:13:37 +0200

    add auth to container + deployment configuration

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -54,11 +54,12 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-export default function Header({ signedIn, onSignOut }) {
+export default function Header({ isSignedIn, onSignOut }) {
   const classes = useStyles();
 
+  console.log(isSignedIn);
   const onClick = () => {
-    if (signedIn && onSignOut) {
+    if (isSignedIn && onSignOut) {
       onSignOut();
     }
   };
@@ -86,10 +87,10 @@ export default function Header({ signedIn, onSignOut }) {
             variant="outlined"
             className={classes.link}
             component={RouterLink}
-            to={signedIn ? "/" : "/auth/signin"}
+            to={isSignedIn ? "/" : "/auth/signin"}
             onClick={onClick}
           >
-            {signedIn ? "Logout" : "Login"}
+            {isSignedIn ? "Logout" : "Login"}
           </Button>
         </Toolbar>
       </AppBar>
