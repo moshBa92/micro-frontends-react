92e52914519a17b29ef3a65e838ed17102d5274d
Moshiko Basal <moshiko@bambidynamic.com>
2022-12-13 16:18:10 +0200

    add Auth and routing inprovment

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,11 +1,15 @@
+import React, { lazy, Suspense } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import {
   StylesProvider,
   createGenerateClassName,
 } from "@material-ui/core/styles";
-import React from "react";
-import { BrowserRouter } from "react-router-dom";
+
 import Header from "./components/Header";
-import MarketingApp from "./components/MarketingApp";
+import Progress from "./components/Progress";
+
+const MarketingLazy = lazy(() => import("./components/MarketingApp"));
+const AuthLazy = lazy(() => import("./components/AuthApp"));
 
 const generateClassName = createGenerateClassName({
   productionPrefix: "co",
@@ -17,8 +21,12 @@ export default () => {
       <StylesProvider generateClassName={generateClassName}>
         <div>
           <Header />
-          <hr />
-          <MarketingApp />
+          <Suspense fallback={<Progress />}>
+            <Switch>
+              <Route path="/auth" component={AuthLazy} />
+              <Route path="/" component={MarketingLazy} />
+            </Switch>
+          </Suspense>
         </div>
       </StylesProvider>
     </BrowserRouter>
